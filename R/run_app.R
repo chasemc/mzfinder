d12478057c0de63b22a5bd2814dd6fcb440fc41c
Chase Clark <18691127+chasemc@users.noreply.github.com>
2021-07-14 13:21:43 -0500

    Update run_app.R

--- R/run_app.R
+++ R/run_app.R
@@ -8,7 +8,7 @@
 run_app <- function(...) {
   with_golem_options(
     app = shinyApp(ui = app_ui,
-                   server = app_server), 
-    golem_opts = list(maxUploadSize=30*1024^2)
+                   server = app_server,maxUploadSize=30*1024^2), 
+    golem_opts = list(...)
   )
 }
